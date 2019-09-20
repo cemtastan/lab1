783ebeeeb3ac5a5503a5408f68eb212bffeb2730
cemtastan <cemtastan1234@gmail.com>
2019-09-21 00:54:25 +0300

    lab 1

---
+++ course1.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+
+
+
+const text = fs.readFileSync("./data.txt").toString();
+ const stringArray = text.split('\.');
+ var sayac = 1;
+stringArray.forEach((element) => {
+    console.log(sayac +'-'+element)
+    sayac++;
+});
